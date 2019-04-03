3bc6926c4cab1e9de2bdc8d3047e6aa747d78500
Gerald <geraldpng@gmail.com>
2019-04-03 22:03:10 +0800

    test consignment-cli

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"log"
 	"os"
@@ -10,6 +11,7 @@ import (
 	pb "github.com/gpng/grpc-tutorial/consignment-service/proto/consignment"
 	microclient "github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
+	"github.com/micro/go-micro/metadata"
 )
 
 const (
@@ -28,6 +30,7 @@ func parseFile(file string) (*pb.Consignment, error) {
 }
 
 func main() {
+
 	cmd.Init()
 
 	// Create new greeter client
@@ -35,9 +38,15 @@ func main() {
 
 	// Contact the server and print out its response.
 	file := defaultFilename
-	if len(os.Args) > 1 {
-		file = os.Args[1]
+	var token string
+	log.Println(os.Args)
+
+	if len(os.Args) < 3 {
+		log.Fatal(errors.New("not enough arguments, expecing file and token"))
 	}
+
+	file = os.Args[1]
+	token = os.Args[2]
 
 	consignment, err := parseFile(file)
 
@@ -45,17 +54,25 @@ func main() {
 		log.Fatalf("Could not parse file: %v", err)
 	}
 
-	r, err := client.CreateConsignment(context.Background(), consignment)
+	// Create a new context which contains our given token.
+	// This same context will be passed into both the calls we make
+	// to our consignment-service.
+	ctx := metadata.NewContext(context.Background(), map[string]string{
+		"token": token,
+	})
+
+	// First call using our tokenised context
+	r, err := client.CreateConsignment(ctx, consignment)
 	if err != nil {
-		log.Fatalf("Could not greet: %v", err)
+		log.Fatalf("Could not create: %v", err)
 	}
 	log.Printf("Created: %t", r.Created)
 
-	getAll, err := client.GetConsignment(context.Background(), &pb.GetRequest{})
+	// Second call
+	getAll, err := client.GetConsignments(ctx, &pb.GetRequest{})
 	if err != nil {
-		log.Fatalf("Coudl not list consignments: %v", err)
+		log.Fatalf("Could not list consignments: %v", err)
 	}
-
 	for _, v := range getAll.Consignments {
 		log.Println(v)
 	}
